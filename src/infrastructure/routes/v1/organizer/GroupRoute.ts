ae136408e1e2b15a91c80e71eb0ba30a751e3d98
Guilherme Pereira <guilhermehnrque@gmail.com>
2024-08-25 14:44:00 -0300

    feat: fix de validadores

--- src/infrastructure/routes/v1/organizer/GroupRoute.ts
+++ src/infrastructure/routes/v1/organizer/GroupRoute.ts
@@ -8,7 +8,7 @@ const groupController = new GroupController();
 
 router.get('', async (req: Request, res: Response) => { groupController.getUserGroupsByUserId(req, res); });
 router.post('', schemas.register, handleValidationErrors, async (req: Request, res: Response) => { groupController.createGroup(req, res); });
-router.get('/:groupId', schemas.detail, async (req: Request, res: Response) => { groupController.getGroupById(req, res); });
-router.put('/:groupId', schemas.detail, async (req: Request, res: Response) => { groupController.updateGroupById(req, res); });
+router.get('/:groupId', schemas.detail, handleValidationErrors, async (req: Request, res: Response) => { groupController.getGroupById(req, res); });
+router.put('/:groupId', schemas.update, handleValidationErrors, async (req: Request, res: Response) => { groupController.updateGroupById(req, res); });
 
 export default router;
